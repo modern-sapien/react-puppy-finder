f08dc82c86a3e8558bddbce428e7207487301dac
ad-fleming <adfleming@gmail.com>
2020-10-28 13:29:21 -0400

    added discover to test routes

---
+++ src/components/Discover.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Discover() {
+  return (
+    <div>
+      <h1>Discover</h1>
+      <p>
+        I display photos and stuff
+      </p>
+      <button>I'm button 1</button>
+      <button>I'm butotn 2</button>
+    </div>
+  );
+}
+
+export default Home;
